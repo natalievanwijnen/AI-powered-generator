5f052b5d9f62691474a402e207c587f96981cc22
natalievanwijnen <154531313+natalievanwijnen@users.noreply.github.com>
2024-03-31 13:45:13 +0200

    Improved UI and ux

--- src/script.js
+++ src/script.js
@@ -35,13 +35,14 @@ function getTextWidth(text) {
 
 // Generate and display poem
 function displayPoem(response) {
+  document.getElementById("loadingMessage").remove();
+
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
     cursor: "",
     deleteSpeed: 40,
     delay: 25,
-    pauseFor: 10000,
     loop: false,
   });
 }
@@ -49,16 +50,19 @@ function displayPoem(response) {
 function generatePoem(event) {
   event.preventDefault();
 
+  document.getElementById("poem").innerHTML =
+    "<p id='loadingMessage'>Scouring the archives...</p>";
+
   let instruction = document.getElementById("generatePoem").value;
   let apiKey = "ofa25a26c683btbc029a13b3d2bf94cc";
   let prompt = `I'm seeking a poem related to "${instruction}".
-  Do not generate a new poem; it must already exist.
+  Do not generate a new poem; the poem must already exist.
   The poem's title must not be revealed.
-  If the poem exceeds 10 lines, please provide an excerpt. 
-  Please strictly adhere to the following format: "poem" - "author". Poem first, author after, never a title.
-  Strongly prioritize works by recognized authors; however, if none exist on this topic, anonymous works must be an exception.
-  Please select poems that are artistic and heartfelt, avoiding mere narrative extracts.
-  I do not want any prompt confirmation like: "Sure, here is an existing poem about..." Or additional context like: "Poem: ..."`;
+  The poem cannot exceed 10 lines. 
+  Please strictly adhere to the following format: "poem" - "poet". Poem first, poet after, never a title.
+  Strongly prioritize works by recognized poets; however, if none exist on this topic, anonymous works can be an exception.
+  Please select poems that are artistic and heartfelt, avoiding mere narrative extracts and quotes.
+  Never provide any prompt confirmation like: "Sure, here is an existing poem about..." Or additional context like: "Poem: ..."`;
 
   let context = `As a holder of a literary degree with expertise in poetry, your task is to present a single piece of poetry.
   Do not provide supplementary information or ask additional questions.
